8f05946116ce8e2b1161ebe33961db390a8ee53b
Bi-Moon <Bi-Moon@192.168.0.117>
2020-09-23 10:09:09 +0700

    graphene_

--- Graphene/src/test/java/Simple_test.java
+++ Graphene/src/test/java/Simple_test.java
@@ -1,26 +1,24 @@
 import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
+
 
 @RunWith(Arquillian.class)
 
 public class Simple_test {
-	//WebDriver firefox  = new FirefoxDriver();
+
 	@Drone
 	WebDriver browser;
-	
-	//WebDriver webdriver = new FirefoxDriver();;
     
 	@Test
     
     public void testOpeningHomePage() {
     	
-		// Maximize the browser window
+
 		//driver.manage().window().maximize();
 		
 		browser.get("http://www.google.com");
